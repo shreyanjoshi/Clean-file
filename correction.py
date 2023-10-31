cc306657d08664d5c820d6a9bcb3994c0ce23ce3
Shreyan Joshi <shreyanjoshi02@gmail.com>
2023-10-31 15:32:09 +0530

    Loaded Gramformer before iterating on the file

--- correction.py
+++ correction.py
@@ -1,36 +1,30 @@
-import re
-from textblob import TextBlob
 from better_profanity import profanity
-import pandas as pd
-import sys
+from textblob import TextBlob
 from gramformer import Gramformer
+import pandas as pd
 
-def remove_punctuation(text):
-    return re.sub(r'[^\w\s]', '', str(text))
+# Define Functions to filter profanity, and correct spelling and grammar in text.
+def add_profanity_prediction(text):
+    return profanity.contains_profanity(text)
 
 def correct_spelling(text):
     return str(TextBlob(text).correct())
 
 def correct_grammar(text):
-    gf = Gramformer(models=1, use_gpu=False) # 1=corrector, 2=detector
     corrected_text = gf.correct(text)
     return corrected_text
 
-def add_profanity_prediction(text):
-    return profanity.contains_profanity(text)
-
 # Read the CSV file into a pandas DataFrame
 input_csv = r"C:\Users\IGTPC\OneDrive\Desktop\sample11thOct.csv" 
-output_csv = "corrected_output1.csv"
-
 df = pd.read_csv(input_csv)
-#df = pd.read_excel(input_csv)
-#df = pd.read_excel(input_csv, engine='openpyxl')
 
 # Create new columns for corrected versions
 df['Corrected Spelling'] = ""
 df['Corrected Grammar'] = ""
 df['Profanity'] = ""
+
+# Load Gramformer to correct grammar
+gf = Gramformer(models=1, use_gpu=False) # 1=corrector, 2=detector
 
 # Apply corrections only for 'User' inputs under the 'Role' column
 for index, row in df.iterrows():
@@ -38,10 +32,12 @@ for index, row in df.iterrows():
         profanity_check = add_profanity_prediction(row['Content'])
         corrected_spelling_text = correct_spelling(row['Content'])
         corrected_grammar_text = correct_grammar(corrected_spelling_text)
+ #       corrected_grammar_text = correct_grammar(row['Content'])
         # Update the new columns with corrected versions
         df.at[index, 'Corrected Spelling'] = corrected_spelling_text
         df.at[index, 'Corrected Grammar'] = corrected_grammar_text
         df.at[index, 'Profanity'] = profanity_check
 
 # Save the corrected data to a new CSV file
+output_csv = "corrected_output1.csv"
 df.to_csv(output_csv, index=False)
