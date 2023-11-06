4b87969721b7233a2ef6f8cba6524e04e41fa886
Shreyan Joshi <shreyanjoshi02@gmail.com>
2023-11-06 16:55:57 +0530

    Censored Profane Content

--- correction.py
+++ correction.py
@@ -7,6 +7,9 @@ import pandas as pd
 def add_profanity_prediction(text):
     return profanity.contains_profanity(text)
 
+def filter_and_remove_profanity(text):
+    return profanity.censor(text)
+
 def correct_spelling(text):
     return str(TextBlob(text).correct())
 
@@ -19,9 +22,10 @@ input_csv = r"C:\Users\IGTPC\OneDrive\Desktop\sample11thOct.csv"
 df = pd.read_csv(input_csv)
 
 # Create new columns for corrected versions
+df['Profanity'] = ""
+df['Censored Text'] = ""
 df['Corrected Spelling'] = ""
 df['Corrected Grammar'] = ""
-df['Profanity'] = ""
 
 # Load Gramformer to correct grammar
 gf = Gramformer(models=1, use_gpu=False) # 1=corrector, 2=detector
@@ -29,14 +33,17 @@ gf = Gramformer(models=1, use_gpu=False) # 1=corrector, 2=detector
 # Apply corrections only for 'User' inputs under the 'Role' column
 for index, row in df.iterrows():
     if row['Role'] == 'User':
-        profanity_check = add_profanity_prediction(row['Content'])
-        corrected_spelling_text = correct_spelling(row['Content'])
+        original_text = row['Content']
+        profanity_check = add_profanity_prediction(original_text)
+        cleaned_text = filter_and_remove_profanity(original_text)
+        corrected_spelling_text = correct_spelling(cleaned_text)
         corrected_grammar_text = correct_grammar(corrected_spelling_text)
         # Update the new columns with corrected versions
+        df.at[index, 'Profanity'] = profanity_check
+        df.at[index, 'Censored Text'] = cleaned_text
         df.at[index, 'Corrected Spelling'] = corrected_spelling_text
         df.at[index, 'Corrected Grammar'] = corrected_grammar_text
-        df.at[index, 'Profanity'] = profanity_check
 
 # Save the corrected data to a new CSV file
-output_csv = "corrected_output1.csv"
+output_csv = "corrected_output.csv"
 df.to_csv(output_csv, index=False)
